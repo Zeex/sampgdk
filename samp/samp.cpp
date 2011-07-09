7a9045e7a819e2c45191db48e4b9931cbd60397b
Sergey Zolotarev <zeex@rocketmail.com>
2011-07-09 23:29:46 +0600

    #pragma should be out of namespace

--- samp/samp.cpp
+++ samp/samp.cpp
@@ -1000,8 +1000,8 @@ bool UpdatePlayer3DTextLabelText(int playerid, int id, long color, const std::st
     return native(&::fakeAmx, params);
 }
 
+} // namespace samp
+
 #ifdef _MSC_VER
     #pragma warning(pop)
 #endif
-
-} // namespace samp
