bf080a71bbcebb7c8f123a21d9e9324de80b6dbc
Zeex <zeex@rocketmail.com>
2011-07-11 22:08:32 +0600

    HelloWorld gamemode

---
+++ helloworld.h
@@ -0,0 +1,14 @@
+#ifndef SAMPLE_GAMEMODE_H
+#define SAMPLE_GAMEMODE_H
+
+#include "samp/samp.h"
+
+class HelloWorld : public samp::EventHandler {
+public:
+    virtual void OnGameModeInit();
+    virtual bool OnPlayerConnect(int playerid);
+    virtual bool OnPlayerRequestClass(int playerid);
+    virtual bool OnPlayerCommandText(int playerid, const std::string &cmdtext);
+};
+
+#endif
