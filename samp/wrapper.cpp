628ce2b27443798e258ceda56ec8af5845faf756
Zeex <zeex@rocketmail.com>
2011-07-13 16:52:53 +0600

    fixed missing #include <cstring>

--- samp/wrapper.cpp
+++ samp/wrapper.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstring> // for memcpy()
 #include <string>
 
 #include "callbacks.h"
