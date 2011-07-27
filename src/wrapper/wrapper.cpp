fcbfee7c91d988804ee92b311e35355773a96350
Sergey Zolotarev <zeex@rocketmail.com>
2011-07-27 08:26:56 +0600

    Make things look better
    - deleted useless comments
    - renamed my_amx_Exec to Exec
    - renamed my_amx_Register to Register
    - renamed my_amx_FindPublic to FindPublic
    - refer to memcpy as std::memcpy

--- src/wrapper/wrapper.cpp
+++ src/wrapper/wrapper.cpp
@@ -29,18 +29,16 @@ extern void *pAMXFunctions;
 static uint32_t amx_Register_addr;
 static unsigned char amx_Register_code[5];
 
-static int my_amx_Register(AMX *amx, AMX_NATIVE_INFO *nativelist, int number) {
-    // Restore the original code so we can call the function
-    memcpy(reinterpret_cast<void*>(::amx_Register_addr), ::amx_Register_code, 5);
+static int Register(AMX *amx, AMX_NATIVE_INFO *nativelist, int number) {
+    std::memcpy(reinterpret_cast<void*>(::amx_Register_addr), ::amx_Register_code, 5);
+
     int error = amx_Register(amx, nativelist, number);
 
-    // Store natives in our global container
     for (int i = 0; nativelist[i].name != 0 && (i < number || number == -1); ++i) {
         sampgdk::Wrapper::GetInstance()->SetNative(nativelist[i].name, nativelist[i].func);
     }
 
-    // Set the jump again to catch further calls
-    SetJump(reinterpret_cast<void*>(::amx_Register_addr), (void*)::my_amx_Register, ::amx_Register_code);
+    SetJump(reinterpret_cast<void*>(::amx_Register_addr), (void*)Register, ::amx_Register_code);
 
     return error;
 }
@@ -51,23 +49,20 @@ static unsigned char amx_FindPublic_code[5];
 static AMX *pGamemode = 0;
 static std::string lastPublicName;
 
-static int my_amx_FindPublic(AMX *amx, const char *name, int *index) {
-    // Restore the original code so we can call the function
-    memcpy(reinterpret_cast<void*>(::amx_FindPublic_addr), ::amx_FindPublic_code, 5);
+static int FindPublic(AMX *amx, const char *name, int *index) {
+    std::memcpy(reinterpret_cast<void*>(::amx_FindPublic_addr), ::amx_FindPublic_code, 5);
+
     int error = amx_FindPublic(amx, name, index);
 
     if (amx == ::pGamemode && ::pGamemode != 0) {
         if (error != AMX_ERR_NONE) {
-            // The requested public doesn't exist but we say it does
-            // to let the server subsequently execute it.
             *index = AMX_EXEC_CHEAT;
             error = AMX_ERR_NONE;
         }
         ::lastPublicName = name;
     }
 
-    // Set the jump again to catch further calls
-    SetJump(reinterpret_cast<void*>(::amx_FindPublic_addr), (void*)::my_amx_FindPublic, ::amx_FindPublic_code);
+    SetJump(reinterpret_cast<void*>(::amx_FindPublic_addr), (void*)FindPublic, ::amx_FindPublic_code);
 
     return error;
 }
@@ -75,14 +70,13 @@ static int my_amx_FindPublic(AMX *amx, const char *name, int *index) {
 static uint32_t amx_Exec_addr;
 static unsigned char amx_Exec_code[5];
 
-static int my_amx_Exec(AMX *amx, cell *retval, int index) {
+static int Exec(AMX *amx, cell *retval, int index) {
+    std::memcpy(reinterpret_cast<void*>(::amx_Exec_addr), ::amx_Exec_code, 5);
+
     bool canDoExec = true;
 
     if (index == AMX_EXEC_MAIN) {
-        // main() is being called => this is the gamemode
         ::pGamemode = amx;
-        // Manually call OnGameModeInit from here as it's usually called 
-        // before main() and is impossible to catch using the current method
         canDoExec = sampgdk::Wrapper::GetInstance()->ExecutePublicHook(::pGamemode, retval, "OnGameModeInit");
     }
 
@@ -92,22 +86,16 @@ static int my_amx_Exec(AMX *amx, cell *retval, int index) {
         }
     } 
 
-    // Restore the original code so we can call the function
-    memcpy(reinterpret_cast<void*>(::amx_Exec_addr), ::amx_Exec_code, 5);
-
     int error = AMX_ERR_NONE;
 
     if (canDoExec) {
         error = amx_Exec(amx, retval, index);
     }
-
     if (!canDoExec || (index == AMX_EXEC_CHEAT && error == AMX_ERR_INDEX)) {
-        // Pop parameters from stack
         amx->stk += amx->paramcount * sizeof(cell);
     }
 
-    // Set the jump again to catch further calls
-    SetJump(reinterpret_cast<void*>(::amx_Exec_addr), (void*)::my_amx_Exec, ::amx_Exec_code);
+    SetJump(reinterpret_cast<void*>(::amx_Exec_addr), (void*)Exec, ::amx_Exec_code);
 
     return error;
 }
@@ -132,23 +120,18 @@ Wrapper *Wrapper::GetInstance() {
 }
 
 void Wrapper::Initialize(void **ppPluginData) {
-    // Very important things
     ::pAMXFunctions = ppPluginData[PLUGIN_DATA_AMX_EXPORTS];
     logprintf = (logprintf_t)ppPluginData[PLUGIN_DATA_LOGPRINTF];
 
-    // Hook amx_Register
     ::amx_Register_addr = reinterpret_cast<uint32_t>((static_cast<void**>(pAMXFunctions))[PLUGIN_AMX_EXPORT_Register]);
-    SetJump(reinterpret_cast<void*>(::amx_Register_addr), (void*)::my_amx_Register, ::amx_Register_code);
+    SetJump(reinterpret_cast<void*>(::amx_Register_addr), (void*)Register, ::amx_Register_code);
 
-    // Hook amx_FindPublic
     ::amx_FindPublic_addr = reinterpret_cast<uint32_t>((static_cast<void**>(pAMXFunctions))[PLUGIN_AMX_EXPORT_FindPublic]);
-    SetJump(reinterpret_cast<void*>(::amx_FindPublic_addr), (void*)::my_amx_FindPublic, ::amx_FindPublic_code);
+    SetJump(reinterpret_cast<void*>(::amx_FindPublic_addr), (void*)FindPublic, ::amx_FindPublic_code);
 
-    // Hook amx_Exec
     ::amx_Exec_addr = reinterpret_cast<uint32_t>((static_cast<void**>(pAMXFunctions))[PLUGIN_AMX_EXPORT_Exec]);
-    SetJump(reinterpret_cast<void*>(::amx_Exec_addr), (void*)::my_amx_Exec, ::amx_Exec_code);
+    SetJump(reinterpret_cast<void*>(::amx_Exec_addr), (void*)Exec, ::amx_Exec_code);
 
-    // Set handlers for all SA:MP callbacks
     sampgdk::InitializeCallbacks();
 }
 
