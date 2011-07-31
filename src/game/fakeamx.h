489fcbf00fc9431f630a82ef2c682d8e172826d8
Zeex <zeex@rocketmail.com>
2011-08-01 04:07:14 +0600

    Add facility to push and pop from fakeAmx heap

--- src/game/fakeamx.h
+++ src/game/fakeamx.h
@@ -5,5 +5,38 @@
 
 extern AMX fakeAmx;
 
+cell FakeAmxPush(void *what, size_t cells);
+void FakeAmxPop(cell address);
+
+class FakeAmxHeapObject {
+public:
+    FakeAmxHeapObject(void *obj, size_t cells) 
+        : size_(cells), address_(FakeAmxPush(obj, cells))
+    {}
+
+    ~FakeAmxHeapObject() { FakeAmxPop(address_); }
+
+    cell address() const { return address_; }
+
+    size_t size() const { return size_; }
+
+    cell asCell() const { 
+        return *reinterpret_cast<cell*>(::fakeAmx.data + address_); 
+    }
+
+    float asFloat() const {
+        cell v = this->asCell();
+        return amx_ctof(v);
+    }
+
+    char *asString() const {
+        return reinterpret_cast<char*>(::fakeAmx.data + address_);
+    }
+
+private:
+    size_t size_;
+    cell address_;
+};
+
 #endif
 
