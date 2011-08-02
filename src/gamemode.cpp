d1181c875aa4c3d0323acb4e626b617238928dde
Zeex <zeex@rocketmail.com>
2011-08-02 23:51:48 +0600

    Put gamemode's AMX* in separate file

---
+++ src/gamemode.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+
+#include "gamemode.h"
+
+static AMX *pGameMode;
+
+AMX *GetGameMode() {
+    return pGameMode;
+}
+
+void SetGameMode(AMX *amx) {
+    assert(amx != 0);
+    ::pGameMode = amx;
+}
