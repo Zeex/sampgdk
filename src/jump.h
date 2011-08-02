60ffc4e86f16e822db1127ad3a1737f7cee9592a
Zeex <zeex@rocketmail.com>
2011-08-02 23:52:37 +0600

    New helper class Jump

--- src/jump.h
+++ src/jump.h
@@ -15,9 +15,33 @@
 #ifndef JUMP_H
 #define JUMP_H
 
+#include <cstring>
+#include <map>
+
 // Modifies code at 'from' to make it JMP to 'to'.
 void SetJump(void *from, void *to);
 void SetJump(void *from, void *to, unsigned char (&oldCode)[5]);
+
+class Jump {
+public:
+    inline void Install(void *from, void *to) {
+        from_ = from; to_ = to;
+        SetJump(from_, to_, code_);
+    }
+
+    inline void Remove() {
+        std::memcpy(from_, code_, 5);
+    }
+
+    inline void Reinstall() {
+        SetJump(from_, to_, code_);
+    }
+
+private:
+    void *from_;
+    void *to_;
+    unsigned char code_[5];
+};
 
 #endif
 
