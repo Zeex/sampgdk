6e2f98f8b01d100984ace9a3540f4c9d6db70fcb
Sergey Zolotarev <zeex@rocketmail.com>
2011-08-05 05:08:26 +0800

    Workarounds for problems with compiling on MinGW

--- src/amxapihooks.cpp
+++ src/amxapihooks.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <sampgdk/plugin.h>
+#include <sampgdk/amxplugin.h>
 #include <sampgdk/wrapper.h>
 
 #include "amxapihooks.h"
