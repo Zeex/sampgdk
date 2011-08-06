871f4b875196c9c35da5a674a46a75cb3a4674a4
Sergey Zolotarev <zeex@rocketmail.com>
2011-08-06 16:34:42 +0600

    Add #include <string>

--- src/amxapihooks.cpp
+++ src/amxapihooks.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <string>
+
 #include <sampgdk/plugin.h>
 #include <sampgdk/wrapper.h>
 
