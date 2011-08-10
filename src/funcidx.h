f87b428b269fc6584e96732473080fe2439f9487
Sergey Zolotarev <zeex@rocketmail.com>
2011-08-10 14:55:05 +0600

    Add funcidx.h

---
+++ src/funcidx.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2011 SA:MP GDK Project
+// See file LICENSE for license details.
+
+#ifndef SAMPGDK_FUNCIDX_H
+#define SAMPGDK_FUNCIDX_H
+
+extern cell AMX_NATIVE_CALL funcidx(AMX *amx, cell *params);
+
+#endif
