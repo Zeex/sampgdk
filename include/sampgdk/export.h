619a17d5d0290a316ab3d57421da2b6e27a02d6e
Zeex <zeex@rocketmail.com>
2011-10-21 18:12:25 +0700

    Insert checks for IN_SAMPGDK and SAMPGDK_STATIC macros

--- include/sampgdk/export.h
+++ include/sampgdk/export.h
@@ -1,12 +1,31 @@
 #ifndef SAMPGDK_EXPORT_H
 #define SAMPGDK_EXPORT_H
 
-#if defined WIN32 || defined _WIN32
-    #define SAMPGDK_EXPORT __declspec(dllexport)
-    #define SAMPGDK_CALL   __stdcall
-#else
-    #define SAMPGDK_EXPORT 
-    #define SAMPGDK_CALL
-#endif 
+#undef SAMPGDK_EXPORT
+#undef SAMPGDK_CALL
 
+#if defined IN_SAMPGDK 
+	#if !defined SAMPGDK_STATIC
+		#if defined WIN32 || defined _WIN32
+    		#define SAMPGDK_EXPORT __declspec(dllexport)
+    		#define SAMPGDK_CALL   __stdcall
+		#else
+    		#define SAMPGDK_EXPORT __attribute__((visibility("default")))
+		#endif 
+	#endif
+#else // !IN_SAMPGDK
+	#if !defined SAMPGDK_STATIC
+		#if defined WIN32 || defined _WIN32
+			#define SAMPGDK_EXPORT __declspec(dllimport)
+		#endif
+	#endif
+#endif // IN_SAMPGDK
+
+#if !defined SAMPGDK_EXPORT
+	#define SAMPGDK_EXPORT
 #endif
+#if !defined SAMPGDK_CALL
+	#define SAMPGDK_CALL
+#endif
+
+#endif // !SAMPGDK_EXPORT_H
