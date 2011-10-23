8a80e4f234de329d1a891b833207e42d378abf75
Sergey Zolotarev <zeex@rocketmail.com>
2011-10-24 01:15:23 +0700

    New export.h

--- include/sampgdk/export.h
+++ include/sampgdk/export.h
@@ -15,18 +15,34 @@
 #ifndef SAMPGDK_EXPORT_H
 #define SAMPGDK_EXPORT_H
 
-#if defined __cplusplus
-	#define SAMPGDK_EXTERN extern "C"
-#else
-	#define SAMPGDK_EXTERN extern
+#undef SAMPGDK_EXPORT
+#undef SAMPGDK_CALL
+
+#if defined _WIN32 || defined WIN32
+	#define SAMPGDK_CALL __stdcall
 #endif
 
-#if defined WIN32 || defined _WIN32
-	#define SAMPGDK_EXPORT SAMPGDK_EXTERN __declspec(dllexport) 
-	#define SAMPGDK_CALL   __cdecl
-#else
-	#define SAMPGDK_EXPORT 
+#if defined IN_SAMPGDK 
+	#if !defined SAMPGDK_STATIC
+		#if defined WIN32 || defined _WIN32
+			#define SAMPGDK_EXPORT __declspec(dllexport)
+		#else
+			#define SAMPGDK_EXPORT __attribute__((visibility("default")))
+		#endif 
+	#endif
+#else // !IN_SAMPGDK
+	#if !defined SAMPGDK_STATIC
+		#if defined WIN32 || defined _WIN32
+			#define SAMPGDK_EXPORT __declspec(dllimport)
+		#endif
+	#endif
+#endif // IN_SAMPGDK
+
+#if !defined SAMPGDK_EXPORT
+	#define SAMPGDK_EXPORT
+#endif
+#if !defined SAMPGDK_CALL
 	#define SAMPGDK_CALL
-#endif 
-
 #endif
+
+#endif // !SAMPGDK_EXPORT_H
