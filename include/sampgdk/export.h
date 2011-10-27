60e44c6fb31f5ab99fb21cc864334f16e52bb753
Sergey Zolotarev <zeex@rocketmail.com>
2011-10-27 18:11:40 +0700

    Revert export.h

--- include/sampgdk/export.h
+++ include/sampgdk/export.h
@@ -15,34 +15,18 @@
 #ifndef SAMPGDK_EXPORT_H
 #define SAMPGDK_EXPORT_H
 
-#undef SAMPGDK_EXPORT
-#undef SAMPGDK_CALL
-
-#if defined _WIN32 || defined WIN32
-	#define SAMPGDK_CALL __stdcall
+#if defined __cplusplus
+	#define SAMPGDK_EXTERN extern "C"
+#else
+	#define SAMPGDK_EXTERN extern
 #endif
 
-#if defined IN_SAMPGDK 
-	#if !defined SAMPGDK_STATIC
-		#if defined WIN32 || defined _WIN32
-			#define SAMPGDK_EXPORT __declspec(dllexport)
-		#else
-			#define SAMPGDK_EXPORT __attribute__((visibility("default")))
-		#endif 
-	#endif
-#else // !IN_SAMPGDK
-	#if !defined SAMPGDK_STATIC
-		#if defined WIN32 || defined _WIN32
-			#define SAMPGDK_EXPORT __declspec(dllimport)
-		#endif
-	#endif
-#endif // IN_SAMPGDK
-
-#if !defined SAMPGDK_EXPORT
-	#define SAMPGDK_EXPORT
-#endif
-#if !defined SAMPGDK_CALL
+#if defined WIN32 || defined _WIN32
+	#define SAMPGDK_EXPORT SAMPGDK_EXTERN __declspec(dllexport) 
+	#define SAMPGDK_CALL   __cdecl
+#else
+	#define SAMPGDK_EXPORT 
 	#define SAMPGDK_CALL
-#endif
+#endif 
 
-#endif // !SAMPGDK_EXPORT_H
+#endif
