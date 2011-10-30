f14476a3f61cfede846799b4eb2679a163614e4f
Zeex <zeex@rocketmail.com>
2011-10-30 13:23:06 +0600

    Fix warning complaining about unused formal parameters

--- src/eventhandler.cpp
+++ src/eventhandler.cpp
@@ -72,167 +72,167 @@ bool EventHandler::OnGameModeExit() {
 	return true;
 }
 
-bool EventHandler::OnPlayerConnect(int playerid) { 
+bool EventHandler::OnPlayerConnect(int /*playerid*/) { 
 	return true; 
 }
 
-bool EventHandler::OnPlayerDisconnect(int playerid, int reason) { 
+bool EventHandler::OnPlayerDisconnect(int /*playerid*/, int /*reason*/) { 
 	return true; 
 }
 
-bool EventHandler::OnPlayerSpawn(int playerid) {
+bool EventHandler::OnPlayerSpawn(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerDeath(int playerid, int killerid, int reason) {
+bool EventHandler::OnPlayerDeath(int /*playerid*/, int /*killerid*/, int /*reason*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleSpawn(int vehicleid) {
+bool EventHandler::OnVehicleSpawn(int /*vehicleid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleDeath(int vehicleid, int killerid) {
+bool EventHandler::OnVehicleDeath(int /*vehicleid*/, int /*killerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerText(int playerid, const std::string &text) {
+bool EventHandler::OnPlayerText(int /*playerid*/, const std::string &/*text*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerCommandText(int playerid, const std::string &cmdtext) {
+bool EventHandler::OnPlayerCommandText(int /*playerid*/, const std::string &/*cmdtext*/) {
 	return false;
 }
 
-bool EventHandler::OnPlayerRequestClass(int playerid, int classid) {
+bool EventHandler::OnPlayerRequestClass(int /*playerid*/, int /*classid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerEnterVehicle(int playerid, int vehicleid, bool ispassenger) {
+bool EventHandler::OnPlayerEnterVehicle(int /*playerid*/, int /*vehicleid*/, bool /*ispassenger*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerExitVehicle(int playerid, int vehicleid) {
+bool EventHandler::OnPlayerExitVehicle(int /*playerid*/, int /*vehicleid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerStateChange(int playerid, int newstate, int oldstate) {
+bool EventHandler::OnPlayerStateChange(int /*playerid*/, int /*newstate*/, int /*oldstate*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerEnterCheckpoint(int playerid) {
+bool EventHandler::OnPlayerEnterCheckpoint(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerLeaveCheckpoint(int playerid) {
+bool EventHandler::OnPlayerLeaveCheckpoint(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerEnterRaceCheckpoint(int playerid) {
+bool EventHandler::OnPlayerEnterRaceCheckpoint(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerLeaveRaceCheckpoint(int playerid) {
+bool EventHandler::OnPlayerLeaveRaceCheckpoint(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnRconCommand(const std::string &cmd) {
+bool EventHandler::OnRconCommand(const std::string &/*cmd*/) {
 	return false;
 }
 
-bool EventHandler::OnPlayerRequestSpawn(int playerid) {
+bool EventHandler::OnPlayerRequestSpawn(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnObjectMoved(int objectid) {
+bool EventHandler::OnObjectMoved(int /*objectid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerObjectMoved(int playerid, int objectid) {
+bool EventHandler::OnPlayerObjectMoved(int /*playerid*/, int /*objectid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerPickUpPickup(int playerid, int pickupid) {
+bool EventHandler::OnPlayerPickUpPickup(int /*playerid*/, int /*pickupid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleMod(int playerid, int vehicleid, int componentid) {
+bool EventHandler::OnVehicleMod(int /*playerid*/, int /*vehicleid*/, int /*componentid*/) {
 	return true;
 }
 
-bool EventHandler::OnEnterExitModShop(int playerid, bool enterexit, int interiorid) {
+bool EventHandler::OnEnterExitModShop(int /*playerid*/, bool /*enterexit*/, int /*interiorid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehiclePaintjob(int playerid, int vehicleid, int paintjobid) {
+bool EventHandler::OnVehiclePaintjob(int /*playerid*/, int /*vehicleid*/, int /*paintjobid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleRespray(int playerid, int vehicleid, int color1, int color2) {
+bool EventHandler::OnVehicleRespray(int /*playerid*/, int /*vehicleid*/, int /*color1*/, int /*color2*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleDamageStatusUpdate(int vehicleid, int playerid) {
+bool EventHandler::OnVehicleDamageStatusUpdate(int /*vehicleid*/, int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnUnoccupiedVehicleUpdate(int vehicleid, int playerid, int passenger_seat) {
+bool EventHandler::OnUnoccupiedVehicleUpdate(int /*vehicleid*/, int /*playerid*/, int /*passenger_seat*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerSelectedMenuRow(int playerid, int row) {
+bool EventHandler::OnPlayerSelectedMenuRow(int /*playerid*/, int /*row*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerExitedMenu(int playerid) {
+bool EventHandler::OnPlayerExitedMenu(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerInteriorChange(int playerid, int newinteriorid, int oldinteriorid) {
+bool EventHandler::OnPlayerInteriorChange(int /*playerid*/, int /*newinteriorid*/, int /*oldinteriorid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerKeyStateChange(int playerid, int newkeys, int oldkeys) {
+bool EventHandler::OnPlayerKeyStateChange(int /*playerid*/, int /*newkeys*/, int /*oldkeys*/) {
 	return true;
 }
 
-bool EventHandler::OnRconLoginAttempt(const std::string &ip, const std::string &password, bool success) {
+bool EventHandler::OnRconLoginAttempt(const std::string &/*ip*/, const std::string &/*password*/, bool /*success*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerUpdate(int playerid) {
+bool EventHandler::OnPlayerUpdate(int /*playerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerStreamIn(int playerid, int forplayerid) {
+bool EventHandler::OnPlayerStreamIn(int /*playerid*/, int /*forplayerid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerStreamOut(int playerid, int forplayerid) {
+bool EventHandler::OnPlayerStreamOut(int /*playerid*/, int /*forplayerid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleStreamIn(int vehicleid, int forplayerid) {
+bool EventHandler::OnVehicleStreamIn(int /*vehicleid*/, int /*forplayerid*/) {
 	return true;
 }
 
-bool EventHandler::OnVehicleStreamOut(int vehicleid, int forplayerid) {
+bool EventHandler::OnVehicleStreamOut(int /*vehicleid*/, int /*forplayerid*/) {
 	return true;
 }
 
-bool EventHandler::OnDialogResponse(int playerid, int dialogid, bool response, int listitem, const std::string &inputtext) {
+bool EventHandler::OnDialogResponse(int /*playerid*/, int /*dialogid*/, bool /*response*/, int /*listitem*/, const std::string &/*inputtext*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerClickPlayer(int playerid, int clickedplayerid, int source) {
+bool EventHandler::OnPlayerClickPlayer(int /*playerid*/, int /*clickedplayerid*/, int /*source*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerTakeDamage(int playerid, int issuerid, float amount, int weaponid) {
+bool EventHandler::OnPlayerTakeDamage(int /*playerid*/, int /*issuerid*/, float /*amount*/, int /*weaponid*/) {
 	return true;
 }
 
-bool EventHandler::OnPlayerGiveDamage(int playerid, int damagedid, float amount, int weaponid) {
+bool EventHandler::OnPlayerGiveDamage(int /*playerid*/, int /*damagedid*/, float /*amount*/, int /*weaponid*/) {
 	return true;
 }
 
