420effd9f5214977bef31907ced097a170ffd823
Zeex <zeex@rocketmail.com>
2011-10-30 22:34:28 +0600

    Build only static library

--- include/sampgdk/export.h
+++ include/sampgdk/export.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2011 Zeex
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SAMPGDK_EXPORT_H
 #define SAMPGDK_EXPORT_H
 
@@ -9,7 +23,7 @@
 #endif
 
 #if defined IN_SAMPGDK 
-	#if !defined SAMPGDK_STATIC
+	#if defined SAMPGDK_SHARED
 		#if defined WIN32 || defined _WIN32
 			#define SAMPGDK_EXPORT __declspec(dllexport)
 		#else
@@ -17,7 +31,7 @@
 		#endif 
 	#endif
 #else // !IN_SAMPGDK
-	#if !defined SAMPGDK_STATIC
+	#if defined SAMPGDK_SHARED
 		#if defined WIN32 || defined _WIN32
 			#define SAMPGDK_EXPORT __declspec(dllimport)
 		#endif
