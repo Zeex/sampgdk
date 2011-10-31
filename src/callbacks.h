5b91f52cbae1feea9e634993e8df187d793ed7f8
Zeex <zeex@rocketmail.com>
2011-10-31 12:07:37 +0600

    Revert "Self-registering callback hooks"
    This reverts commit 0050163514b9403fee96b57eea4cf8cdaa0f3904.
    
    Conflicts:
    
    	src/callbacks.cpp
    	src/wrapperimpl.cpp

---
+++ src/callbacks.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2011 SA:MP GDK Project
+// See file LICENSE for license details.
+
+#ifndef SAMP_CALLBACKS_H
+#define SAMP_CALLBACKS_H
+
+namespace sampgdk { 
+
+void SetupSampCallbackHooks();
+
+} 
+
+#endif
+
