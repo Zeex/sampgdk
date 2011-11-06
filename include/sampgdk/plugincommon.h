aa10fbe56999be1767738515f0277d0557c0f8ce
Zeex <zeex@rocketmail.com>
2011-11-06 17:32:48 +0600

    Add SUPPORTS_PROCESS_TICK  = 0x20000

--- include/sampgdk/plugincommon.h
+++ include/sampgdk/plugincommon.h
@@ -48,6 +48,7 @@ enum SUPPORTS_FLAGS
 	SUPPORTS_VERSION		= SAMP_PLUGIN_VERSION,
 	SUPPORTS_VERSION_MASK	= 0xffff,
 	SUPPORTS_AMX_NATIVES	= 0x10000,
+	SUPPORTS_PROCESS_TICK	= 0x20000
 };
 
 //----------------------------------------------------------
