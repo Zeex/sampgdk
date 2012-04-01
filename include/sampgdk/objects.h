08e9e30e86efaf59108221ba3695b4b8b806b355
Sergey Zolotarev <zeex@rocketmail.com>
2012-04-02 02:20:04 +0600

    Restore old headers for backwards compatibility but warn when they used

---
+++ include/sampgdk/objects.h
@@ -0,0 +1,9 @@
+#ifndef SAMPGDK_OBJECTS_H
+#define SAMPGDK_OBJECTS_H
+
+#pragma message ("The header \"objects.h\" is deprecated and will be removed in futre versions. "\
+                 "Use \"a_objects.h\" instead.")
+
+#include <sampgdk/a_objects.h>
+
+#endif /* SAMPGDK_OBJECTS_H */
---
+++ include/sampgdk/samp.h
@@ -0,0 +1,9 @@
+#ifndef SAMPGDK_SAMP_H
+#define SAMPGDK_SAMP_H
+
+#pragma message ("The header \"samp.h\" is deprecated and will be removed in futre versions. "\
+                 "Use \"a_samp.h\" instead.")
+
+#include <sampgdk/a_samp.h>
+
+#endif /* SAMPGDK_SAMP_H */
