ba0c98d43f645ce63f9a3b68d3c3e56fcc6a0a81
Zeex <zeex@rocketmail.com>
2012-04-02 02:20:04 +0600

    Restore old headers for backwards compatibility but warn when they used

---
+++ include/sampgdk/players.h
@@ -0,0 +1,9 @@
+#ifndef SAMPGDK_PLAYERS_H
+#define SAMPGDK_PLAYERS_H
+
+#pragma message ("The header \"players.h\" is deprecated and will be removed in futre versions. "\
+                 "Use \"a_players.h\" instead.")
+
+#include <sampgdk/a_players.h>
+
+#endif /* SAMPGDK_PLAYERS_H */
---
+++ include/sampgdk/vehicles.h
@@ -0,0 +1,9 @@
+#ifndef SAMPGDK_VEHICLES_H
+#define SAMPGDK_VEHICLES_H
+
+#pragma message ("The header \"vehicles.h\" is deprecated and will be removed in futre versions. "\
+                 "Use \"a_vehicles.h\" instead.")
+
+#include <sampgdk/a_vehicles.h>
+
+#endif /* SAMPGDK_VEHICLES_H */
