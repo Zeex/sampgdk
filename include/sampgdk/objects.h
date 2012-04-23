0d5548f7b95bc39658bb5a4bd14b90a3d479fb22
Sergey Zolotarev <zeex@rocketmail.com>
2012-04-23 21:14:24 +0600

    Delete deprecated headers
    It's gonna be a new major release so fuck compatibility...

--- include/sampgdk/objects.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPGDK_OBJECTS_H
-#define SAMPGDK_OBJECTS_H
-
-#pragma message ("The header \"objects.h\" is deprecated and will be removed in futre versions. "\
-                 "Use \"a_objects.h\" instead.")
-
-#include <sampgdk/a_objects.h>
-
-#endif /* SAMPGDK_OBJECTS_H */
--- include/sampgdk/players.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPGDK_PLAYERS_H
-#define SAMPGDK_PLAYERS_H
-
-#pragma message ("The header \"players.h\" is deprecated and will be removed in futre versions. "\
-                 "Use \"a_players.h\" instead.")
-
-#include <sampgdk/a_players.h>
-
-#endif /* SAMPGDK_PLAYERS_H */
