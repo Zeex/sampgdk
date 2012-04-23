68a14d874fc0ace3279ebb320a8645d79d68080c
Zeex <zeex@rocketmail.com>
2012-04-23 21:14:24 +0600

    Delete deprecated headers
    It's gonna be a new major release so fuck compatibility...

--- include/sampgdk/samp.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPGDK_SAMP_H
-#define SAMPGDK_SAMP_H
-
-#pragma message ("The header \"samp.h\" is deprecated and will be removed in futre versions. "\
-                 "Use \"a_samp.h\" instead.")
-
-#include <sampgdk/a_samp.h>
-
-#endif /* SAMPGDK_SAMP_H */
--- include/sampgdk/vehicles.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPGDK_VEHICLES_H
-#define SAMPGDK_VEHICLES_H
-
-#pragma message ("The header \"vehicles.h\" is deprecated and will be removed in futre versions. "\
-                 "Use \"a_vehicles.h\" instead.")
-
-#include <sampgdk/a_vehicles.h>
-
-#endif /* SAMPGDK_VEHICLES_H */
