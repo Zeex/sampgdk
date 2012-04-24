6d2066eb357f24535c06be0edb17ffc017415f11
Zeex <zeex@rocketmail.com>
2012-04-24 14:01:51 +0600

    Delete plugin.h

--- include/sampgdk/plugin.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPGDK_PLUGIN_H
-#define SAMPGDK_PLUGIN_H
-#pragma once
-
-#include <sampgdk/amx.h>
-#include <sampgdk/core.h>
-#include <sampgdk/plugincommon.h>
-
-#endif /* !SAMPGDK_PLUGIN_H */
