e10acef1b0da7a604f0eb8ef8c9c9d2df3af79f1
Zeex <zeex@rocketmail.com>
2012-05-02 02:52:46 +0600

    nativegen: Make use of parse_header

--- scripts/nativegen.py
+++ scripts/nativegen.py
@@ -7,6 +7,8 @@ import os
 import re
 import sys
 
+from parse_header import *
+
 class InvalidNativeArgumentType:
 	def __init__(self, type):
 		self.type = type
@@ -17,46 +19,18 @@ class NativeArgument:
 		self.type = match.group(1).strip()
 		self.name = match.group(2).strip()
 
-def parse_argument_list(arg_list):
-	""" For each entry of the arg_list returns a tuple made of
-	    argument type and name. """
-	for string in arg_list:
-		if len(string) == 0:
-			continue
-		yield NativeArgument(string)
+def to_argument_list(args):
+	arg_list = []
+	for type, name in args:
+		yield type + " " + name
 
-def parse_attributes(string):
-	""" Parse generator attributes. Each attribute is a key=value pair
-	    separated by commas. """
-	attrs = dict()
-	if string != None:
-		items = string.split(",")
-		for item in items:
-			nv = item.split("=")
-			if len(nv) > 1:
-				attrs[nv[0].strip()] = nv[1].strip()
-			else:
-				attrs[nv[0].strip()] = None
-	return attrs
-
-def get_comment_text(comment):
-	""" Extracts text in /* ... */ comments (C-style comments). """
-	text = comment
-	text = re.sub("^\s*/\*\s*", "", text)
-	text = re.sub("\s*\*/\s*$", "", text)
-	return text
-
-def generate_native_code(return_type, name, arg_list, comment):
-	if comment is not None:
-		comment = get_comment_text(comment)
-	attrs = parse_attributes(comment)
-
+def generate_native_code(return_type, name, args, attrs):
 	if "$skip" in attrs:
 		return None
 
 	# Write first line, same as function declaration + "{\n".
 	code = "SAMPGDK_EXPORT " + return_type + " SAMPGDK_CALL sampgdk_" + name\
-		+ "(" + ", ".join(arg_list) + ") {\n"
+		+ "(" + ", ".join(to_argument_list(args)) + ") {\n"
 
 	# A "static" variable that holds native address.
 	if "$real_name" in attrs:
@@ -66,57 +40,56 @@ def generate_native_code(return_type, name, arg_list, comment):
 	code += "\tstatic AMX_NATIVE native = sampgdk::Natives::GetInstance().GetNativeWarn(\"" + real_name + "\");\n"
 
 	# Generate code for local variables, params array and ref argument assignment.
-	args = list(parse_argument_list(arg_list))
 	if len(args) > 0:
 		locals_code = ""
 		params_code = "\tcell params[] = {\n\t\t0"
 		assign_code = ""
 		expect_buffer_size = False
-		for arg in args:
+		for type, name in args:
 			# Local FakeAmxHeapObject instances.
 			if expect_buffer_size:
-				locals_code += arg.name + ");\n"
-				assign_code += arg.name + ");\n"
+				locals_code += name + ");\n"
+				assign_code += name + ");\n"
 				expect_buffer_size = False
-			if arg.type.endswith("*"):
-				locals_code += "\tsampgdk::FakeAmxHeapObject " + arg.name + "_"
-				if arg.type == "char *":
+			if type.endswith("*"):
+				locals_code += "\tsampgdk::FakeAmxHeapObject " + name + "_"
+				if type == "char *":
 					# Output string buffer whose size is passed via next argument.
 					locals_code += "("
 					expect_buffer_size = True
-				elif arg.type == "const char *":
+				elif type == "const char *":
 					# Constant string.
-					locals_code += "(" + arg.name + ");\n"
+					locals_code += "(" + name + ");\n"
 				else:
 					# Other output parameters.
 					locals_code += ";\n"
 			# The "params" array.
 			params_code += ",\n\t\t"
-			if arg.type == "int" or arg.type == "bool":
-				params_code += arg.name
-			elif arg.type == "float":
-				params_code += "amx_ftoc(" + arg.name + ")"
-			elif arg.type.endswith("*"):
-				params_code += arg.name + "_.address()"
+			if type == "int" or type == "bool":
+				params_code += name
+			elif type == "float":
+				params_code += "amx_ftoc(" + name + ")"
+			elif type.endswith("*"):
+				params_code += name + "_.address()"
 			else:
-				raise InvalidNativeArgumentType(arg.type)
+				raise InvalidNativeArgumentType(type)
 			# Assignment of pointer arguments.
-			if arg.type.endswith("*"):
-				if arg.type == "const char *":
+			if type.endswith("*"):
+				if type == "const char *":
 					pass
-				elif arg.type == "char *":
-					assign_code += "\t" + arg.name + "_.GetAsString(" + arg.name + ", "
+				elif type == "char *":
+					assign_code += "\t" + name + "_.GetAsString(" + name + ", "
 					expect_buffer_size = True
 				else:
-					assign_code += "\t*" + arg.name + " = " + arg.name + "_."
-					if arg.type == "int *":
+					assign_code += "\t*" + name + " = " + name + "_."
+					if type == "int *":
 						assign_code += "Get();\n"
-					elif arg.type == "bool *":
+					elif type == "bool *":
 						assign_code += "GetAsBool();\n"
-					elif arg.type == "float *":
+					elif type == "float *":
 						assign_code += "GetAsFloat();\n"
 					else:
-						raise InvalidNativeArgumentType(arg.type)
+						raise InvalidNativeArgumentType(type)
 		params_code += "\n\t};\n"
 		code += locals_code + params_code + assign_code
 
@@ -134,23 +107,10 @@ def generate_native_code(return_type, name, arg_list, comment):
 	code += "}\n"
 	return code
 
-def generate_native_macro(return_type, name, arg_list, comment):
+def generate_native_macro(return_type, name, args, comment):
 	code = "#undef " + name + "\n"
 	code += "#define " + name + " sampgdk_" + name + "\n"
 	return code
-
-def process_native_decl(string):
-	""" Returns a tuple of (type, name, arg_list) where arg_list is a
-	    list of arguments and each argument is a string that consists
-	    of an argument type and name (no default values can occur). """
-	match = re.match(r"SAMPGDK_EXPORT (int|bool|float) SAMPGDK_CALL (\w+)\((.*)\);\s*(/\*.*$)?", string)
-	if match == None:
-		return None
-	return_type = match.group(1)
-	name = match.group(2)
-	arg_list = match.group(3).split(", ")
-	comment = match.group(4)
-	return (return_type, name, arg_list, comment)	
 
 def process_file(header_file, source_file, macros_file):
 	""" Processes a C/C++ header file "src" finding native function
@@ -158,19 +118,14 @@ def process_file(header_file, source_file, macros_file):
 	header = open(header_file, "r")
 	source = open(source_file, "w")
 	macros = open(macros_file, "w")
-	for line in header:
-		line = line.strip()
-		if len(line) == 0:
-			continue
+	for native in parse_header(header.read()):
 		try:
-			native = process_native_decl(line)
-			if native is not None:
-				native_macro = generate_native_macro(*native)
-				if native_macro is not None:
-					macros.write(native_macro + "\n")
-				native_code = generate_native_code(*native)
-				if native_code is not None:
-					source.write(native_code + "\n")
+			native_macro = generate_native_macro(*native)
+			if native_macro is not None:
+				macros.write(native_macro + "\n")
+			native_code = generate_native_code(*native)
+			if native_code is not None:
+				source.write(native_code + "\n")
 		except InvalidNativeArgumentType as arg:
 			sys.stderr.write("Invalid argument type '" + arg.type + "' in declaration:\n" + line + "\n")
 	header.close()
