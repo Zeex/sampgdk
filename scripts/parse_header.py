f14406c506abe6c13a0b42488a95e1c3b57308d2
Zeex <zeex@rocketmail.com>
2012-05-02 02:11:23 +0600

    parse_header: Fix comment

--- scripts/parse_header.py
+++ scripts/parse_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# parse_header.py finds all exported exports in a GDK header and writes them 
+# parse_header.py finds all exported functions in a GDK header and writes them 
 # to a nice XML file. That file can be used to e.g. generate code for SA-MP natives.
 
 import os
