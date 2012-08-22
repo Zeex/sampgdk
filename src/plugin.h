5f6191d16830a22291f6ea4699d4940e250c85e7
Zeex <zeex@rocketmail.com>
2012-08-23 00:08:22 +0700

    Some documentation

--- src/plugin.h
+++ src/plugin.h
@@ -23,14 +23,38 @@ struct plugin_list {
 	struct plugin_list *next;
 };
 
+/* Registers a plugin (for notifying it about SA-MP events, etc). If the
+ * plugin happens to be already registered nothing no action taken.
+ *
+ * This functions walks through the list of registered plugins and compares
+ * their handles with the given one so it's O(n).
+ */
 void plugin_register(void *plugin);
+
+/* Opposite of plugin_register(). 
+ */
 bool plugin_unregister(void *plugin);
+
+/* Checks if a plugin was registered with plugin_register().
+ */
 bool plugin_is_registered(void *plugin);
+
+/* Returns address of a symbol (variable or function) or NULL if not found.
+ */
 void *plugin_find_symbol(void *plugin, const char *name);
 
+/* Returns a handle of the module (plugin) to which the specified address
+ * belongs to.
+ */
 void *plugin_address_to_handle(void *address);
+
+/* Returns a full path of the module (plugin) to which the specified address
+ * belongs to.
+ */ 
 void plugin_address_to_filename(void *address, char *filename, size_t size);
 
+/* Returns the list of currently registered plugins. 
+ */
 struct plugin_list *plugin_get_list();
 
 #endif /* !SAMPGDK_PLUGIN_H_ */
