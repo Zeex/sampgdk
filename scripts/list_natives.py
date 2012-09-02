9dcea7b1ec658608bae72bd5cf33358e43c2fbb3
Zeex <zeex@rocketmail.com>
2012-09-02 11:10:26 +0700

    Pass native name prefix as an argument to list_natives.py

--- scripts/list_natives.py
+++ scripts/list_natives.py
@@ -5,8 +5,13 @@ import sys
 from parse_header import *
 
 def main(argv):
+	prefix = ""
+
+	if len(argv) > 1:
+		prefix = argv[1]
+
 	for type, name, args, attrs in get_natives(sys.stdin.read()):
-		print "sampgdk_" + name
+		print prefix + name
 
 if __name__ == "__main__":
 	main(sys.argv)
