c56e636270de2f486141167ae08b69b99add642a
Zeex <zeex@rocketmail.com>
2012-09-05 14:37:30 +0700

    Make get_param_c_type() handle unknown types better
    In case the requested type could not be matched to any C type
    the rule is as follows:
    
    1. if 'in', return the type name as is
    2. if 'out', append '*' (star) to the type name (i.e. make it a pointer)
    3. otherwise return None

--- src/codegen.py
+++ src/codegen.py
@@ -36,6 +36,22 @@ idl_to_c_type_out = {
 	'string' : 'char **'
 }
 
+def get_param_c_type(p):
+	try:
+		if p.is_out():
+			return idl_to_c_type_out[p.type]
+		if p.is_in():
+			return idl_to_c_type_in[p.type]
+	except KeyError:
+		try:
+			if p.is_out():
+				return '%s *' % p.type
+			if p.is_in():
+				return p.type
+		except KeyError:
+			pass
+	return None
+
 def value_to_c_literal(v):
 	if v.is_bool():
 		return ('false', 'true')[v.data]
@@ -49,13 +65,6 @@ def value_to_c_literal(v):
 		return '\'%s\'' % v.data
 	elif v.is_string():
 		return '"%s"' % v.data
-	return None
-
-def get_param_c_type(p):
-	if p.is_in():
-		return idl_to_c_type_in[p.type]
-	elif p.is_out():
-		return idl_to_c_type_out[p.type]
 	return None
 
 def params_to_string(params):
