7fa40468665ee1a1ffc6e7e18d2caa7c3f114756
Zeex <zeex@rocketmail.com>
2012-09-13 20:25:43 +0700

    Rewrite of SampPlugin module

--- cmake/Modules/SampPlugin.cmake
+++ cmake/Modules/SampPlugin.cmake
@@ -1,13 +1,7 @@
-# SampPlugin.cmake - code common to most of SA:MP plugins.
+# SampPlugin.cmake
 #
-# This module makes use of the following variables:
-#   PLUGIN_NAME           The plugin name. Set to ${CMAKE_PROJECT_NAME} by default.
-#   PLUGIN_SOURCES        The list of source files used to build the plugin.
-#   PLUGIN_DEF            A full path to a module definition (.def) file.
-#                         Set to ${CMAKE_CURRENT_SOURCE_DIR}/${PLUGIN_NAME}.def by default.
-#
-#  In turn, this module defines the following variables:
-#    PLUGIN_TARGET        The name of the target used to compile the plugin.
+# Functions:
+#   add_samp_plugin(name [sources...])
 #
 # Supported platforms:
 #   Windows
@@ -20,47 +14,19 @@
 
 include(AmxConfig)
 
-if(NOT DEFINED PLUGIN_NAME)
-	set(PLUGIN_NAME "${CMAKE_PROJECT_NAME}")
-endif()
-if(PLUGIN_NAME STREQUAL "")
-	message(FATAL_ERROR "At least one of PLUGIN_NAME/CMAKE_PROEJCT_NAME must be set")
-endif()
+function(add_samp_plugin name)
+	add_library(${name} MODULE ${ARGN})
 
-if(NOT DEFINED PLUGIN_SOURCES OR PLUGIN_SOURCES STREQUAL "")
-	message(FATAL_ERROR "PLUGIN_SOURCES is not defined")
-endif()	
-
-if(NOT DEFINED PLUGIN_DEF OR PLUGIN_DEF STREQUAL "")
-	set(PLUGIN_DEF "${CMAKE_CURRENT_SOURCE_DIR}/${PLUGIN_NAME}.def")
-	if(NOT EXISTS ${PLUGIN_DEF})
-		set(PLUGIN_DEF "")
+	if(WIN32)
+		set_target_properties(${name} PROPERTIES PREFIX "")
 	endif()
-endif()
 
-set(PLUGIN_TARGET ${PLUGIN_NAME})
-
-add_library(${PLUGIN_TARGET} MODULE ${PLUGIN_SOURCES})
-
-set_target_properties(${PLUGIN_TARGET} PROPERTIES PREFIX "")
-
-if(DEFINED PLUGIN_DEF)
-	if(MSVC)
-		set_target_properties(${PLUGIN_TARGET} PROPERTIES 
-				LINK_FLAGS "/DEF:${PLUGIN_DEF}")
-	elseif(MINGW)
-		set_target_properties(${PLUGIN_TARGET} PROPERTIES
-				LINK_FLAGS "-Wl,--kill-at --def ${PLUGIN_DEF}")
+	if(CMAKE_COMPILER_IS_GNUCC)
+		set_property(TARGET ${name} APPEND PROPERTY COMPILE_FLAGS "-m32")
+		set_property(TARGET ${name} APPEND PROPERTY LINK_FLAGS    "-m32")
 	endif()
-endif()
 
-if(UNIX)
-	add_definitions(-DLINUX)
-endif()
-if(WIN32)
-	add_definitions(-DWIN32)
-endif()
-
-if(CMAKE_COMPILER_IS_GNUCXX)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
-endif()
+	if(UNIX AND NOT WIN32)
+		set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "LINUX")
+	endif()
+endfunction()
