9da0e0987b3777b24f72342414d8da6185b60775
Zeex <zeex@rocketmail.com>
2013-01-12 14:11:05 +0700

    Cut module path from GlobalBuildConfig and rename it to BuildConfig

--- cmake/BuildConfig.cmake
+++ cmake/BuildConfig.cmake
@@ -1,11 +1,6 @@
 # This file is included only if sampgdk is built as a root project (i.e. not
 # as a subdirectory).
 
-list(APPEND CMAKE_MODULE_PATH
-	"${CMAKE_SOURCE_DIR}"
-	"${CMAKE_SOURCE_DIR}/cmake/Modules"
-)
-
 if(${CMAKE_C_COMPILER_ID} STREQUAL "Clang" OR ${CMAKE_C_COMPILER_ID} STREQUAL "Intel")
 	set(CMAKE_COMPILER_IS_GNUCC TRUE)
 endif()
