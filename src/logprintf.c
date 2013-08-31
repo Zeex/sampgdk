3d49a4f8d971668d6e6639adde54f8e345baf3d9
Zeex <zeex@rocketmail.com>
2013-09-01 05:43:01 +0700

    Simpler solution for the format escaping problem
    Instead of passing the buffer as a format string to sampgdk_logprintf(), pass
    it as an argument of %s, which will simply ignore remaining specifiers.

--- src/logprintf.c
+++ src/logprintf.c
@@ -28,40 +28,8 @@
   #define vsnprintf vsprintf_s
 #endif
 
-static char *escape_format_string(const char *format) {
-  int i, j;
-  int nr_percents;
-  char *new_format;
-
-  nr_percents = 0;
-  for (i = 0; format[i] != '\0'; i++) {
-    if (format[i] == '%') {
-      nr_percents++;
-    }
-  }
-
-  new_format = calloc(nr_percents + i + 1, sizeof(char));
-  if (new_format != NULL) {
-    for (i = 0, j = 0; format[i] != '\0'; i++, j++) {
-      new_format[j] = format[i];
-      if (format[i] == '%') {
-        new_format[++j] = '%';
-      }
-    }
-  }
-
-  return new_format;
-}
-
 void sampgdk_do_vlogprintf(const char *format, va_list va) {
   char buffer[LOGPRINTF_BUFFER_SIZE];
-  char *escaped;
-
   vsnprintf(buffer, sizeof(buffer), format, va);
-  escaped = escape_format_string(buffer);
-
-  if (escaped != NULL) {
-    sampgdk_logprintf(escaped);
-    free(escaped);
-  }
+  sampgdk_logprintf("%s", buffer);
 }
