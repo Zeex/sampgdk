1ec38d5803dac1f5d446e85576a42b4dfa33dc86
Zeex <zeex@rocketmail.com>
2013-11-10 05:23:51 +0700

    Slightly simplify helloworld code

--- plugins/helloworld/helloworld.cpp
+++ plugins/helloworld/helloworld.cpp
@@ -1,6 +1,5 @@
-#include <cstdio>  // for sprintf
-#include <cstring> // for strcmp
-
+#include <cstdio>
+#include <cstring>
 #include <sampgdk/a_players.h>
 #include <sampgdk/a_samp.h>
 #include <sampgdk/core.h>
@@ -14,15 +13,8 @@ static void SAMPGDK_TIMER_CALL RepeatingTimer(int, void *) {
 
 PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit() {
   SetGameModeText("Hello, World!");
-
   AddPlayerClass(0, 1958.3783f, 1343.1572f, 15.3746f, 269.1425f, 0, 0, 0, 0, 0, 0);
-
-  ServerLog::Printf("------------------------------------------\n");
-  ServerLog::Printf("      HelloWorld gamemode got loaded.     \n");
-  ServerLog::Printf("------------------------------------------\n");
-
   SetTimer(1000, true, RepeatingTimer, 0);
-
   return true;
 }
 
@@ -42,25 +34,11 @@ PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid, const char *cmd
   if (std::strcmp(cmdtext, "/hello") == 0) {
     char name[MAX_PLAYER_NAME];
     GetPlayerName(playerid, name);
-
     char message[128];
     std::sprintf(message, "Hello, %s!", name);
-
     SendClientMessage(playerid, 0x00FF00FF, message);
     return true;
   }
-
-  if (std::strcmp(cmdtext, "/pos") == 0) {
-    float x, y, z;
-    GetPlayerPos(playerid, &x, &y, &z);
-
-    char message[128];
-    std::sprintf(message, "You are at (%f, %f, %f)", x, y, z);
-
-    SendClientMessage(playerid, 0xFFFFFFFF, message);
-    return true;
-  }
-
   return false;
 }
 
