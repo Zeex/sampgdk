7ddbea4f4c21bc9dc687fade6208b7c4248c71ee
Zeex <zeex@rocketmail.com>
2013-11-26 13:50:11 +0700

    Revert "Move sampgdk_get_arg_* to a more appropriate place"
    This reverts commit c54148434fbe0588f5b4cd36742e7f6977bb3226.

---
+++ src/arg.c
@@ -0,0 +1,66 @@
+/* Copyright (C) 2012-2013 Zeex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+
+#include "arg.h"
+
+static unsigned char *get_arg_data_ptr(AMX *amx) {
+  return amx->data != 0 
+    ? amx->data 
+    : amx->base + ((AMX_HEADER*)amx->base)->dat;
+}
+
+static cell *get_arg_stack_ptr(AMX *amx) {
+  return (cell*)(get_arg_data_ptr(amx) + amx->stk);
+}
+
+cell sampgdk_get_arg_cell(AMX *amx, int index) {
+  return get_arg_stack_ptr(amx)[index];
+}
+
+bool sampgdk_get_arg_bool(AMX *amx, int index) {
+  return (bool)sampgdk_get_arg_cell(amx, index);
+}
+
+float sampgdk_get_arg_float(AMX *amx, int index) {
+  cell value;
+
+  value = sampgdk_get_arg_cell(amx, index);
+  return amx_ctof(value);
+}
+
+char *sampgdk_get_arg_string(AMX *amx, int index) {
+  cell amx_addr;
+  cell *phys_addr;
+  int length;
+  char *string;
+  
+  amx_addr = sampgdk_get_arg_cell(amx, index);
+  if (amx_GetAddr(amx, amx_addr, &phys_addr) != AMX_ERR_NONE) {
+    return NULL;
+  }
+  
+  amx_StrLen(phys_addr, &length);
+  string = malloc((length + 1) * sizeof(char));
+  
+  if (amx_GetString(string, phys_addr, 0, length + 1) != AMX_ERR_NONE) {
+    free(string);
+    return NULL;
+  }
+
+  return string;
+}
+
