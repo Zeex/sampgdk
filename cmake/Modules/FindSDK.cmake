6d8130b1f466efd343be57a1dbc08c69108f8289
Zeex <zeex@rocketmail.com>
2014-01-14 22:25:23 +0700

    Add more SDK path hints
    FindSDK now considers the SDK_DIR and SAMP_SDK_DIR as well as corresponding
    environment variables.
    
    Also add "sdk" to PATH_SUFFIXES.

--- cmake/Modules/FindSDK.cmake
+++ cmake/Modules/FindSDK.cmake
@@ -1,8 +1,11 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(SDK_INCLUDE_DIR NAMES "plugin.h" "plugincommon.h"
-          PATHS ENV "SAMP_SDK_DIR"
-          PATH_SUFFIXES "SDK"
+find_path(SDK_INCLUDE_DIR NAMES plugin.h plugincommon.h
+          HINTS ${SAMP_SDK_DIR}
+                ENV SAMP_SDK_DIR
+                ${SDK_DIR} 
+                ENV SDK_DIR
+          PATH_SUFFIXES sdk SDK
           DOC "Path to SA-MP plugin SDK"
           NO_SYSTEM_ENVIRONMENT_PATH
           NO_CMAKE_SYSTEM_PATH)
