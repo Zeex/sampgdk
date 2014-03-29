432e312d4c1129dfc87de9dc22fddaa1e1cfd189
Zeex <zeex@rocketmail.com>
2014-03-29 10:50:55 +0700

    Revert "Generate C++ wrappers around SA-MP natives"
    This reverts commit 1121b5641d34f09c3e8d12f686907e1e5b8f9b61.

--- plugins/helloworld/helloworld.cpp
+++ plugins/helloworld/helloworld.cpp
@@ -25,15 +25,15 @@ PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid) {
 PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestClass(int playerid, int classid) {
   SetPlayerPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
   SetPlayerCameraPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
-  SetPlayerCameraLookAt(playerid, 1958.3783f, 1343.1572f, 15.3746f, sampgdk::CAMERA_CUT);
+  SetPlayerCameraLookAt(playerid, 1958.3783f, 1343.1572f, 15.3746f, CAMERA_CUT);
   return true;
 }
 
 PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid, const char *cmdtext) {
   if (strcmp(cmdtext, "/hello") == 0) {
-    char name[sampgdk::MAX_PLAYER_NAME];
+    char name[MAX_PLAYER_NAME];
     GetPlayerName(playerid, name, sizeof(name));
-    char message[sampgdk::MAX_CLIENT_MESSAGE];
+    char message[MAX_CLIENT_MESSAGE];
     sprintf(message, "Hello, %s!", name);
     SendClientMessage(playerid, 0x00FF00FF, message);
     return true;
