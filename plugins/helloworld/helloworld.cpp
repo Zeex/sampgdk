3bb2e15f724b48a332b55ca76762e5afe60df8ad
Zeex <zeex@rocketmail.com>
2014-07-13 16:41:58 +0100

    Limit code to 80 columns in helloworld

--- plugins/helloworld/helloworld.cpp
+++ plugins/helloworld/helloworld.cpp
@@ -14,7 +14,8 @@ void SAMPGDK_CALL PrintTickCountTimer(int timerid, void *params) {
 
 PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit() {
   SetGameModeText("Hello, World!");
-  AddPlayerClass(0, 1958.3783f, 1343.1572f, 15.3746f, 269.1425f, 0, 0, 0, 0, 0, 0);
+  AddPlayerClass(0, 1958.3783f, 1343.1572f, 15.3746f, 269.1425f,
+                 0, 0, 0, 0, 0, 0);
   SetTimer(1000, true, PrintTickCountTimer, 0);
   return true;
 }
@@ -24,14 +25,17 @@ PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid) {
   return true;
 }
 
-PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestClass(int playerid, int classid) {
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestClass(int playerid,
+                                                    int classid) {
   SetPlayerPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
   SetPlayerCameraPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
-  SetPlayerCameraLookAt(playerid, 1958.3783f, 1343.1572f, 15.3746f, CAMERA_CUT);
+  SetPlayerCameraLookAt(playerid, 1958.3783f, 1343.1572f, 15.3746f,
+                        CAMERA_CUT);
   return true;
 }
 
-PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid, const char *cmdtext) {
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid,
+                                                   const char *cmdtext) {
   if (strcmp(cmdtext, "/hello") == 0) {
     char name[MAX_PLAYER_NAME];
     GetPlayerName(playerid, name, sizeof(name));
