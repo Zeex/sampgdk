Zeex/sampgdk

commit 0dc90c645e0d73d1c92a51e8efd20021ac399a10

Reset amx->paramcount before doing callback

src/internal/amxhooks.c
@@ -162,6 +162,11 @@ static int AMXAPI _sampgdk_amxhooks_Exec(AMX *amx, cell *retval, int index) {
   int error = AMX_ERR_NONE;
   int paramcount = amx->paramcount;
 
+  /* We have to reset amx->paramcount at this point so that if the callback
+   * itself calls amx_Exec() it won't pop out arguments off the stack.
+   */
+  amx->paramcount = 0;
+
   /* Since filterscripts don't use main() we can assume that the AMX
    * that executes main() is indeed the main AMX i.e. the gamemode.
    */
@@ -186,10 +191,11 @@ static int AMXAPI _sampgdk_amxhooks_Exec(AMX *amx, cell *retval, int index) {
   sampgdk_hook_install(_sampgdk_amxhooks_Callback_hook);
 
   if (proceed && index != AMX_EXEC_GDK) {
+    amx->paramcount = paramcount;
     error = amx_Exec(amx, retval, index);
   } else {
-    amx->stk += paramcount * sizeof(cell);
     amx->paramcount = 0;
+    amx->stk += paramcount * sizeof(cell);
   }
 
   sampgdk_hook_remove(_sampgdk_amxhooks_Callback_hook);

commit 1906cd8e68324b9461572959816187a8e0abf5fa

Fix bad params in OnPublicCall()
This fixes a bug introduced in commit 0dc90c645e0d73d1c92a51e8efd20021ac399a10.

src/internal/amxhooks.c
@@ -163,7 +163,7 @@ static int AMXAPI _sampgdk_amxhooks_Exec(AMX *amx, cell *retval, int index) {
   int paramcount = amx->paramcount;
 
   /* We have to reset amx->paramcount at this point so that if the callback
-   * itself calls amx_Exec() it won't pop out arguments off the stack.
+   * itself calls amx_Exec() it won't pop our arguments off the stack.
    */
   amx->paramcount = 0;
 
@@ -175,7 +175,7 @@ static int AMXAPI _sampgdk_amxhooks_Exec(AMX *amx, cell *retval, int index) {
      * from being called twice in a row after a gmx.
      */
     if (_sampgdk_amxhooks_main_amx != amx && amx != NULL) {
-      sampgdk_callback_invoke(amx, "OnGameModeInit", retval);
+      sampgdk_callback_invoke(amx, "OnGameModeInit", paramcount, retval);
       _sampgdk_amxhooks_main_amx = amx;
     }
   } else {
@@ -183,7 +183,7 @@ static int AMXAPI _sampgdk_amxhooks_Exec(AMX *amx, cell *retval, int index) {
                                    amx == sampgdk_fakeamx_amx())) {
       char name[_SAMPGDK_AMXHOOKS_MAX_PUBLIC_NAME];
       sampgdk_strcpy(name, _sampgdk_amxhooks_public_name, sizeof(name));
-      proceed = sampgdk_callback_invoke(amx, name, retval);
+      proceed = sampgdk_callback_invoke(amx, name, paramcount, retval);
     }
   }
 

src/internal/callback.c
@@ -189,7 +189,10 @@ void sampgdk_callback_uncache_plugin(void *plugin) {
   }
 }
 
-bool sampgdk_callback_invoke(AMX *amx, const char *name, cell *retval) {
+bool sampgdk_callback_invoke(AMX *amx,
+                             const char *name,
+                             int paramcount,
+                             cell *retval) {
   struct _sampgdk_callback_info *callback;
   struct _sampgdk_callback_info *callback_filter;
   int index;
@@ -206,13 +209,13 @@ bool sampgdk_callback_invoke(AMX *amx, const char *name, cell *retval) {
   assert(callback == NULL
          || callback_filter->cache.count == callback->cache.count);
 
-  if (amx->paramcount > _SAMPGDK_CALLBACK_MAX_ARGS) {
+  if (paramcount > _SAMPGDK_CALLBACK_MAX_ARGS) {
     sampgdk_log_error("Too many callback arguments (at most %d allowed)",
                       _SAMPGDK_CALLBACK_MAX_ARGS);
     return true;
   }
 
-  params[0] = amx->paramcount * sizeof(cell);
+  params[0] = paramcount * sizeof(cell);
   memcpy(&params[1], sampgdk_param_get_start(amx), params[0]);
 
   for (index = 0; index < callback_filter->cache.count; index++) {

src/internal/callback.h
@@ -25,6 +25,7 @@ int sampgdk_callback_register(const char *name, sampgdk_callback handler);
 void sampgdk_callback_unregister(const char *name);
 void sampgdk_callback_cache_plugin(void *plugin);
 void sampgdk_callback_uncache_plugin(void *plugin);
-bool sampgdk_callback_invoke(AMX *amx, const char *name, cell *retval);
+bool sampgdk_callback_invoke(AMX *amx, const char *name,
+    int paramcount, cell *retval);
 
 #endif /* !SAMPGDK_INTERNAL_CALLBACK_H */

src/internal/timer.h
@@ -22,7 +22,7 @@
 typedef void (SAMPGDK_CALL *sampgdk_timer_callback)(int id, void *param);
 
 int sampgdk_timer_set(long interval, bool repeat,
-                      sampgdk_timer_callback calback, void *param);
+    sampgdk_timer_callback calback, void *param);
 int sampgdk_timer_kill(int timerid);
 void sampgdk_timer_process_timers(void *plugin);
 long sampgdk_timer_now(void);