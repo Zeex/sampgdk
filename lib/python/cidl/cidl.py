297e17d9a24805f1af7409f41fed41a54fec7fea
Zeex <zeex@rocketmail.com>
2014-11-08 16:37:55 +0600

    Fix cidl error with negative floats

--- lib/python/cidl/cidl.py
+++ lib/python/cidl/cidl.py
@@ -360,7 +360,7 @@ class Parser(object):
     return t
 
   def t_FLOAT(self, t):
-    r'(([0-9]*\.[0-9]*)([eE][+-]?[0-9]+)?|[0-9]+[eE][+-][0-9]+)'
+    r'[+-]?(([0-9]*\.[0-9]*)([eE][+-]?[0-9]+)?|[0-9]+[eE][+-][0-9]+)'
     t.value = float(t.value)
     return t
 
