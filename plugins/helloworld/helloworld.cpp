3c3f52a10ad22a11f689d70926616df5470fa1cd
Zeex <zeex@rocketmail.com>
2014-11-10 14:55:38 +0600

    Remove test code from helloworld

--- plugins/helloworld/helloworld.cpp
+++ plugins/helloworld/helloworld.cpp
@@ -6,82 +6,58 @@
 #include <sampgdk/core.h>
 #include <sampgdk/sdk.h>
 
-using sampgdk::logprintf;
+using namespace sampgdk;
 
-extern void *pAMXFunctions;
+void SAMPGDK_CALL PrintTickCountTimer(int timerid, void *params) {
+  logprintf("Tick count: %d", GetTickCount());
+}
 
-AMX * a;
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit() {
+  SetGameModeText("Hello, World!");
+  AddPlayerClass(0, 1958.3783f, 1343.1572f, 15.3746f, 269.1425f,
+                 0, 0, 0, 0, 0, 0);
+  SetTimer(1000, true, PrintTickCountTimer, 0);
+  return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid) {
+  SendClientMessage(playerid, 0xFFFFFFFF, "Welcome to the HelloWorld server!");
+  return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestClass(int playerid,
+                                                    int classid) {
+  SetPlayerPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
+  SetPlayerCameraPos(playerid, 1958.3783f, 1343.1572f, 15.3746f);
+  SetPlayerCameraLookAt(playerid, 1958.3783f, 1343.1572f, 15.3746f, CAMERA_CUT);
+  return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid,
+                                                   const char *cmdtext) {
+  if (strcmp(cmdtext, "/hello") == 0) {
+    char name[MAX_PLAYER_NAME];
+    GetPlayerName(playerid, name, sizeof(name));
+    char message[MAX_CLIENT_MESSAGE];
+    sprintf(message, "Hello, %s!", name);
+    SendClientMessage(playerid, 0x00FF00FF, message);
+    return true;
+  }
+  return false;
+}
+
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
+  return sampgdk::Supports() | SUPPORTS_PROCESS_TICK;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
+  return sampgdk::Load(ppData);
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL Unload() {
+  sampgdk::Unload();
+}
 
 PLUGIN_EXPORT void PLUGIN_CALL ProcessTick() {
   sampgdk::ProcessTick();
 }
-
-PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
-{
-    return sampgdk::Supports() | SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
-}
-
-PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
-{
-    pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
-    return sampgdk::Load(ppData);
-}
-
-PLUGIN_EXPORT void PLUGIN_CALL Unload()
-{
-    sampgdk::Unload();
-}
-
-
-PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx)
-{
-    a = amx;
-    return AMX_ERR_NONE;
-}
-
-
-PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx)
-{
-    return AMX_ERR_NONE;
-}
-
-PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid) {
-    int one = 10;
-    int two = 20;
-    int three = 30;
-    int amxIndex = 0;
-
-    logprintf("amx_FindPublic...TestCB3");
-    if (!amx_FindPublic(a, "TestCB3", &amxIndex))
-    {
-        logprintf("amx_Push...");
-        amx_Push(a, one);
-        amx_Push(a, two);
-        amx_Push(a, three);
-
-        logprintf("amx_Exec...");
-        amx_Exec(a, NULL, amxIndex);
-    }
-    else
-    {
-        logprintf("amx_FindPublic failed to find :(...");
-    }
-    return true;
-}
-
-PLUGIN_EXPORT bool PLUGIN_CALL OnPublicCall(AMX *amx, const char *name, cell *params, cell *retval) {
-    if (!strcmp(name, "TestCB1")) {
-        logprintf(">TestCB1(%d) %d", (params[0] / sizeof(cell)), params[1]);
-    }
-    if (!strcmp(name, "TestCB2")) {
-        logprintf(">TestCB2(%d) %d %d", (params[0] / sizeof(cell)), params[1], params[2]);
-    }
-    if (!strcmp(name, "TestCB3")) {
-        logprintf(">TestCB3(%d) %d %d %d", (params[0] / sizeof(cell)), params[1], params[2], params[3]);
-    }
-    if (!strcmp(name, "OnPlayerConnect")) {
-        logprintf(">OnPlayerConnect(%d) %d", (params[0] / sizeof(cell)), params[1]);
-    }
-    return true;
-}
-
